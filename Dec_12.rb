7ee91d18a91cc4cba81c30e27c6b5bcc06246d60
Jacobleakley <jacob.bleakley@gmail.com>
2019-12-08 11:05:15 -0500

    playing with arrays and hashes

---
+++ Dec_12.rb
@@ -0,0 +1,37 @@
+Wardrobe = {
+ 
+"shoes" => {
+    "colors" => ["black", "brown", "blue"],
+    "brands" => ["florsheim", "rockport", "ecco"],
+    "type"   => ["casual", "dress", "running"]
+},
+    "shirts" => {
+        "color"  => ["green", "pink","blue"],
+        "brands" => ["adonis", "AIStone", "brooksBrothers"],
+        "type"   => ["casual", "dress", "evening"]
+    },
+
+        "pants" => {
+            "color"  => ["navy", "black", "grey"],
+            "brands" => ["perryEllis", "calvinKlein", "landsEnd"],
+            "type"   => ["dress", "comfortable" "sweat"]
+}
+}
+
+# puts Wardrobe["shirts"]["color"][1]
+
+# Wardrobe["shirts"]["color"][1]= "red"
+
+# # puts Wardrobe["shirts"]["color"][1]
+
+# Wardrobe["shirts"]["color"][1]= "purple"
+
+# puts Wardrobe["shirts"]["color"][1]
+
+# Wardrobe["shirts"]["brands"]= "purple"
+
+# Wardrobe["shirts"]["size"] = ["small", "medium", "party size"]
+# puts Wardrobe
+
+# Wardrobe["shirts"]["color"][3] = "yellow"
+# puts Wardrobe
